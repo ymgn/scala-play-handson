67385db0b3fa5eb35314b2b49284480916ca5f25
ymgn <gonyamagon@gmail.com>
2020-01-09 00:55:01 +0900

    update Todoを新規追加するための画面が表示されるようにした

--- app/controllers/TodoController.scala
+++ app/controllers/TodoController.scala
@@ -1,6 +1,8 @@
 package controllers
 
 import javax.inject._
+import play.api.data._
+import play.api.data.Forms._
 import play.api.mvc._
 import service._
 
@@ -15,5 +17,19 @@ extends MessagesAbstractController(mcc){
   def list() = Action { implicit request: MessagesRequest[AnyContent] =>
     val items: Seq[Todo] = todoService.list()
     Ok(views.html.list(items))
+  }
+
+  // name項目が必須のFormを作成
+  val todoForm: Form[String] = Form("name" -> nonEmptyText)
+  // todoを新規登録する画面
+  def todoNew() = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok(views.html.createForm(todoForm))
+  }
+
+  // 新規登録画面から叩かれるtodo追加エンドポイント
+  def todoAdd() = Action { implicit  request: MessagesRequest[AnyContent] =>
+    val name: String = todoForm.bindFromRequest().get
+    println(name)
+    Ok("Save")
   }
 }
